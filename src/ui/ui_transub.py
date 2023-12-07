debcb9c8bf17b51833fb615ecdf9ad7c2a3b15db
Gatorix <caosheng0000@outlook.com>
2023-12-07 22:03:45 +0800

    modify pages

--- src/ui/ui_transub.py
+++ src/ui/ui_transub.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'transub.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.4.1
+## Created by: Qt User Interface Compiler version 6.5.1
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -17,23 +17,23 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QGridLayout,
     QGroupBox, QLabel, QLineEdit, QProgressBar,
-    QPushButton, QSizePolicy, QWidget)
+    QPushButton, QSizePolicy, QToolButton, QWidget)
 
 class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(300, 450)
+        Form.resize(300, 470)
         self.settings_group = QGroupBox(Form)
         self.settings_group.setObjectName(u"settings_group")
-        self.settings_group.setGeometry(QRect(10, 90, 281, 281))
-        self.widget = QWidget(self.settings_group)
-        self.widget.setObjectName(u"widget")
-        self.widget.setGeometry(QRect(10, 120, 257, 31))
-        self.gridLayout_2 = QGridLayout(self.widget)
+        self.settings_group.setGeometry(QRect(10, 70, 281, 321))
+        self.layoutWidget = QWidget(self.settings_group)
+        self.layoutWidget.setObjectName(u"layoutWidget")
+        self.layoutWidget.setGeometry(QRect(10, 120, 261, 31))
+        self.gridLayout_2 = QGridLayout(self.layoutWidget)
         self.gridLayout_2.setObjectName(u"gridLayout_2")
         self.gridLayout_2.setContentsMargins(0, 0, 0, 0)
-        self.combo_box_encoding = QComboBox(self.widget)
+        self.combo_box_encoding = QComboBox(self.layoutWidget)
         self.combo_box_encoding.addItem("")
         self.combo_box_encoding.addItem("")
         self.combo_box_encoding.addItem("")
@@ -43,44 +43,44 @@ class Ui_Form(object):
 
         self.gridLayout_2.addWidget(self.combo_box_encoding, 0, 1, 1, 1)
 
-        self.label_encoding = QLabel(self.widget)
+        self.label_encoding = QLabel(self.layoutWidget)
         self.label_encoding.setObjectName(u"label_encoding")
 
         self.gridLayout_2.addWidget(self.label_encoding, 0, 0, 1, 1)
 
-        self.widget1 = QWidget(self.settings_group)
-        self.widget1.setObjectName(u"widget1")
-        self.widget1.setGeometry(QRect(10, 20, 261, 101))
-        self.gridLayout_9 = QGridLayout(self.widget1)
+        self.layoutWidget1 = QWidget(self.settings_group)
+        self.layoutWidget1.setObjectName(u"layoutWidget1")
+        self.layoutWidget1.setGeometry(QRect(10, 20, 261, 101))
+        self.gridLayout_9 = QGridLayout(self.layoutWidget1)
         self.gridLayout_9.setObjectName(u"gridLayout_9")
         self.gridLayout_9.setContentsMargins(0, 0, 0, 0)
-        self.check_box_archive = QCheckBox(self.widget1)
+        self.check_box_archive = QCheckBox(self.layoutWidget1)
         self.check_box_archive.setObjectName(u"check_box_archive")
 
         self.gridLayout_9.addWidget(self.check_box_archive, 0, 0, 1, 1)
 
-        self.check_box_ignore_error = QCheckBox(self.widget1)
+        self.check_box_ignore_error = QCheckBox(self.layoutWidget1)
         self.check_box_ignore_error.setObjectName(u"check_box_ignore_error")
 
         self.gridLayout_9.addWidget(self.check_box_ignore_error, 1, 0, 1, 1)
 
-        self.check_box_extract_text = QCheckBox(self.widget1)
+        self.check_box_extract_text = QCheckBox(self.layoutWidget1)
         self.check_box_extract_text.setObjectName(u"check_box_extract_text")
 
         self.gridLayout_9.addWidget(self.check_box_extract_text, 2, 0, 1, 1)
 
-        self.widget2 = QWidget(self.settings_group)
-        self.widget2.setObjectName(u"widget2")
-        self.widget2.setGeometry(QRect(10, 200, 191, 31))
-        self.gridLayout_5 = QGridLayout(self.widget2)
+        self.layoutWidget2 = QWidget(self.settings_group)
+        self.layoutWidget2.setObjectName(u"layoutWidget2")
+        self.layoutWidget2.setGeometry(QRect(10, 200, 245, 31))
+        self.gridLayout_5 = QGridLayout(self.layoutWidget2)
         self.gridLayout_5.setObjectName(u"gridLayout_5")
         self.gridLayout_5.setContentsMargins(0, 0, 0, 0)
-        self.label_chinese = QLabel(self.widget2)
+        self.label_chinese = QLabel(self.layoutWidget2)
         self.label_chinese.setObjectName(u"label_chinese")
 
         self.gridLayout_5.addWidget(self.label_chinese, 0, 0, 1, 1)
 
-        self.combo_box_chinese = QComboBox(self.widget2)
+        self.combo_box_chinese = QComboBox(self.layoutWidget2)
         self.combo_box_chinese.addItem("")
         self.combo_box_chinese.addItem("")
         self.combo_box_chinese.addItem("")
@@ -88,79 +88,99 @@ class Ui_Form(object):
 
         self.gridLayout_5.addWidget(self.combo_box_chinese, 0, 1, 1, 1)
 
-        self.widget3 = QWidget(self.settings_group)
-        self.widget3.setObjectName(u"widget3")
-        self.widget3.setGeometry(QRect(10, 240, 122, 31))
-        self.gridLayout = QGridLayout(self.widget3)
+        self.layoutWidget3 = QWidget(self.settings_group)
+        self.layoutWidget3.setObjectName(u"layoutWidget3")
+        self.layoutWidget3.setGeometry(QRect(10, 280, 161, 31))
+        self.gridLayout = QGridLayout(self.layoutWidget3)
         self.gridLayout.setObjectName(u"gridLayout")
         self.gridLayout.setContentsMargins(0, 0, 0, 0)
-        self.label_convert = QLabel(self.widget3)
+        self.label_convert = QLabel(self.layoutWidget3)
         self.label_convert.setObjectName(u"label_convert")
 
         self.gridLayout.addWidget(self.label_convert, 0, 0, 1, 1)
 
-        self.combo_box_convert = QComboBox(self.widget3)
+        self.combo_box_convert = QComboBox(self.layoutWidget3)
         self.combo_box_convert.addItem("")
         self.combo_box_convert.addItem("")
         self.combo_box_convert.setObjectName(u"combo_box_convert")
 
         self.gridLayout.addWidget(self.combo_box_convert, 0, 1, 1, 1)
 
-        self.button_custom = QPushButton(self.settings_group)
-        self.button_custom.setObjectName(u"button_custom")
-        self.button_custom.setGeometry(QRect(164, 240, 111, 31))
-        self.widget4 = QWidget(self.settings_group)
-        self.widget4.setObjectName(u"widget4")
-        self.widget4.setGeometry(QRect(10, 160, 201, 31))
-        self.gridLayout_4 = QGridLayout(self.widget4)
+        self.layoutWidget4 = QWidget(self.settings_group)
+        self.layoutWidget4.setObjectName(u"layoutWidget4")
+        self.layoutWidget4.setGeometry(QRect(10, 240, 171, 31))
+        self.gridLayout_4 = QGridLayout(self.layoutWidget4)
         self.gridLayout_4.setObjectName(u"gridLayout_4")
         self.gridLayout_4.setContentsMargins(0, 0, 0, 0)
-        self.label_offset = QLabel(self.widget4)
+        self.label_offset = QLabel(self.layoutWidget4)
         self.label_offset.setObjectName(u"label_offset")
 
         self.gridLayout_4.addWidget(self.label_offset, 0, 0, 1, 1)
 
-        self.line_edit_offset = QLineEdit(self.widget4)
+        self.line_edit_offset = QLineEdit(self.layoutWidget4)
         self.line_edit_offset.setObjectName(u"line_edit_offset")
 
         self.gridLayout_4.addWidget(self.line_edit_offset, 0, 1, 1, 1)
 
+        self.widget = QWidget(self.settings_group)
+        self.widget.setObjectName(u"widget")
+        self.widget.setGeometry(QRect(10, 160, 261, 31))
+        self.gridLayout_7 = QGridLayout(self.widget)
+        self.gridLayout_7.setObjectName(u"gridLayout_7")
+        self.gridLayout_7.setContentsMargins(0, 0, 0, 0)
+        self.label_chinese_2 = QLabel(self.widget)
+        self.label_chinese_2.setObjectName(u"label_chinese_2")
+
+        self.gridLayout_7.addWidget(self.label_chinese_2, 0, 0, 1, 1)
+
+        self.combo_box_chinese_2 = QComboBox(self.widget)
+        self.combo_box_chinese_2.addItem("")
+        self.combo_box_chinese_2.setObjectName(u"combo_box_chinese_2")
+
+        self.gridLayout_7.addWidget(self.combo_box_chinese_2, 0, 1, 1, 1)
+
+        self.toolButton = QToolButton(self.widget)
+        self.toolButton.setObjectName(u"toolButton")
+
+        self.gridLayout_7.addWidget(self.toolButton, 0, 2, 1, 1)
+
         self.folder_group = QGroupBox(Form)
         self.folder_group.setObjectName(u"folder_group")
-        self.folder_group.setGeometry(QRect(10, 10, 281, 71))
-        self.widget5 = QWidget(self.folder_group)
-        self.widget5.setObjectName(u"widget5")
-        self.widget5.setGeometry(QRect(10, 30, 261, 23))
-        self.gridLayout_3 = QGridLayout(self.widget5)
+        self.folder_group.setGeometry(QRect(10, 0, 281, 71))
+        self.layoutWidget5 = QWidget(self.folder_group)
+        self.layoutWidget5.setObjectName(u"layoutWidget5")
+        self.layoutWidget5.setGeometry(QRect(10, 30, 261, 23))
+        self.gridLayout_3 = QGridLayout(self.layoutWidget5)
         self.gridLayout_3.setObjectName(u"gridLayout_3")
         self.gridLayout_3.setContentsMargins(0, 0, 0, 0)
-        self.label_path = QLabel(self.widget5)
+        self.label_path = QLabel(self.layoutWidget5)
         self.label_path.setObjectName(u"label_path")
 
         self.gridLayout_3.addWidget(self.label_path, 0, 0, 1, 1)
 
-        self.line_edit_path = QLineEdit(self.widget5)
+        self.line_edit_path = QLineEdit(self.layoutWidget5)
         self.line_edit_path.setObjectName(u"line_edit_path")
+        self.line_edit_path.setReadOnly(True)
 
         self.gridLayout_3.addWidget(self.line_edit_path, 0, 1, 1, 1)
 
-        self.widget6 = QWidget(Form)
-        self.widget6.setObjectName(u"widget6")
-        self.widget6.setGeometry(QRect(10, 380, 281, 61))
-        self.gridLayout_6 = QGridLayout(self.widget6)
+        self.layoutWidget6 = QWidget(Form)
+        self.layoutWidget6.setObjectName(u"layoutWidget6")
+        self.layoutWidget6.setGeometry(QRect(10, 400, 285, 66))
+        self.gridLayout_6 = QGridLayout(self.layoutWidget6)
         self.gridLayout_6.setObjectName(u"gridLayout_6")
         self.gridLayout_6.setContentsMargins(0, 0, 0, 0)
-        self.label_status = QLabel(self.widget6)
+        self.label_status = QLabel(self.layoutWidget6)
         self.label_status.setObjectName(u"label_status")
 
         self.gridLayout_6.addWidget(self.label_status, 0, 0, 1, 1)
 
-        self.button_start = QPushButton(self.widget6)
+        self.button_start = QPushButton(self.layoutWidget6)
         self.button_start.setObjectName(u"button_start")
 
         self.gridLayout_6.addWidget(self.button_start, 0, 1, 1, 1)
 
-        self.progress_bar = QProgressBar(self.widget6)
+        self.progress_bar = QProgressBar(self.layoutWidget6)
         self.progress_bar.setObjectName(u"progress_bar")
         self.progress_bar.setAutoFillBackground(False)
         self.progress_bar.setValue(24)
@@ -169,7 +189,7 @@ class Ui_Form(object):
 
         self.gridLayout_6.addWidget(self.progress_bar, 1, 0, 1, 1)
 
-        self.button_cancel = QPushButton(self.widget6)
+        self.button_cancel = QPushButton(self.layoutWidget6)
         self.button_cancel.setObjectName(u"button_cancel")
 
         self.gridLayout_6.addWidget(self.button_cancel, 1, 1, 1, 1)
@@ -202,10 +222,13 @@ class Ui_Form(object):
         self.combo_box_convert.setItemText(0, QCoreApplication.translate("Form", u"*.ass", None))
         self.combo_box_convert.setItemText(1, QCoreApplication.translate("Form", u"*.srt", None))
 
-        self.button_custom.setText(QCoreApplication.translate("Form", u"Custom Style", None))
         self.label_offset.setText(QCoreApplication.translate("Form", u"Subtitle Offset ", None))
         self.line_edit_offset.setText("")
         self.line_edit_offset.setPlaceholderText(QCoreApplication.translate("Form", u"0.0", None))
+        self.label_chinese_2.setText(QCoreApplication.translate("Form", u"Custom *.ass Info", None))
+        self.combo_box_chinese_2.setItemText(0, QCoreApplication.translate("Form", u"None", None))
+
+        self.toolButton.setText(QCoreApplication.translate("Form", u"+", None))
         self.folder_group.setTitle(QCoreApplication.translate("Form", u"Step 1: Select folder", None))
         self.label_path.setText(QCoreApplication.translate("Form", u"Source Path", None))
         self.label_status.setText(QCoreApplication.translate("Form", u"Click \"Start\" when you're ready.", None))
--- src/ui/ui_transub_custom_info.py
+++ src/ui/ui_transub_custom_info.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'transub_custom_info.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.4.1
+## Created by: Qt User Interface Compiler version 6.5.1
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -15,8 +15,8 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QPlainTextEdit, QPushButton,
-    QSizePolicy, QWidget)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QLabel, QLineEdit,
+    QPlainTextEdit, QPushButton, QSizePolicy, QWidget)
 
 class Ui_Form(object):
     def setupUi(self, Form):
@@ -25,22 +25,38 @@ class Ui_Form(object):
         Form.resize(400, 300)
         self.plainTextEdit = QPlainTextEdit(Form)
         self.plainTextEdit.setObjectName(u"plainTextEdit")
-        self.plainTextEdit.setGeometry(QRect(10, 10, 381, 241))
-        self.widget = QWidget(Form)
-        self.widget.setObjectName(u"widget")
-        self.widget.setGeometry(QRect(230, 260, 158, 31))
-        self.gridLayout = QGridLayout(self.widget)
+        self.plainTextEdit.setGeometry(QRect(10, 40, 381, 211))
+        self.layoutWidget = QWidget(Form)
+        self.layoutWidget.setObjectName(u"layoutWidget")
+        self.layoutWidget.setGeometry(QRect(230, 260, 158, 32))
+        self.gridLayout = QGridLayout(self.layoutWidget)
         self.gridLayout.setObjectName(u"gridLayout")
         self.gridLayout.setContentsMargins(0, 0, 0, 0)
-        self.pushButton = QPushButton(self.widget)
+        self.pushButton = QPushButton(self.layoutWidget)
         self.pushButton.setObjectName(u"pushButton")
 
         self.gridLayout.addWidget(self.pushButton, 0, 0, 1, 1)
 
-        self.pushButton_2 = QPushButton(self.widget)
+        self.pushButton_2 = QPushButton(self.layoutWidget)
         self.pushButton_2.setObjectName(u"pushButton_2")
 
         self.gridLayout.addWidget(self.pushButton_2, 0, 1, 1, 1)
+
+        self.widget = QWidget(Form)
+        self.widget.setObjectName(u"widget")
+        self.widget.setGeometry(QRect(10, 2, 231, 31))
+        self.gridLayout_2 = QGridLayout(self.widget)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.gridLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.label = QLabel(self.widget)
+        self.label.setObjectName(u"label")
+
+        self.gridLayout_2.addWidget(self.label, 0, 0, 1, 1)
+
+        self.lineEdit = QLineEdit(self.widget)
+        self.lineEdit.setObjectName(u"lineEdit")
+
+        self.gridLayout_2.addWidget(self.lineEdit, 0, 1, 1, 1)
 
 
         self.retranslateUi(Form)
@@ -69,5 +85,6 @@ class Ui_Form(object):
 " Style: DefaultVCD, Arial,28,11861244,11861244,11861244,-2147483640,-1,0,1,1,2,2,30,30,30,0,0", None))
         self.pushButton.setText(QCoreApplication.translate("Form", u"Save", None))
         self.pushButton_2.setText(QCoreApplication.translate("Form", u"Close", None))
+        self.label.setText(QCoreApplication.translate("Form", u"Info Name: ", None))
     # retranslateUi
 
