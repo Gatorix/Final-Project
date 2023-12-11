f11e01529b92cdb5ab9c27b3a4b9f2ef1712512a
Gatorix <caosheng0000@outlook.com>
2023-12-11 18:15:34 +0800

    improve get_all_filepath

--- src/utils/files.py
+++ src/utils/files.py
@@ -1,5 +1,6 @@
 import os
 import sys
+
 from charset_normalizer import detect
 
 
@@ -18,15 +19,11 @@ def get_current_path():
 
 
 def get_all_filepath(folder):
-    # 获取指定路径下的所有.ass文件
+    # 获取指定路径下的所有*.ass, *.srt, *.vtt文件
     file_path = []
     for fpath, dirs, fs in os.walk(folder):
         for f in fs:
-            if '.DS_Store' in os.path.join(fpath, f):
-                pass
-            elif os.path.join(fpath, f)[-4:] != '.ass':
-                pass
-            else:
+            if f.split('.')[-1] in ['srt', 'ass', 'vtt']:
                 file_path.append(os.path.join(fpath, f))
     return file_path
 
@@ -34,3 +31,11 @@ def get_all_filepath(folder):
 def get_file_encoding(file):
     with open(file, 'rb') as f:
         return detect(f.read())['encoding']
+
+# if __name__ == '__main__':
+#     p = get_all_filepath(
+#         r'C:\Users\kingdee\Desktop\Final-Project\test\S02')
+#     print(p)
+#     print(len(p))
+#     t=r'C:\\Users\\kingdee\\Desktop\\Final-Project\\test\\Killers'
+#     print(t.split('.')[-1])
